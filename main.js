eb929819972939bae85cd1782abc1d8657d9ecf6
wustve <wu.stve@gmail.com>
2020-08-21 14:24:57 -0400

    add image to template

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 app.get('/', (req, res) => {
      //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
-     res.render('index.ejs', {fish : 'sharker'})
+     res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679'})
 })
 
 app.listen(process.env.PORT || 3000, () => {
