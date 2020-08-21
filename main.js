b3274d78276490be2e59a95c39517378958da8c6
wustve <wu.stve@gmail.com>
2020-08-21 15:40:54 -0400

    add menu from array

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const app = express()
 const ejs = require('ejs')
 
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 require("dotenv").config()
 
 app.get('/', (req, res) => {
      //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
-     res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679'})
+     res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679', options :[1,2,3]})
 })
 
 app.listen(process.env.PORT || 3000, () => {
