8d91133260273370fe8809fef4d294c3b8eef192
wustve <wu.stve@gmail.com>
2020-08-21 14:16:39 -0400

    initial template

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
+const ejs = require('ejs')
 
+app.set('view engine', 'ejs')
 require("dotenv").config()
 
 app.get('/', (req, res) => {
-     res.send("test!")
+     //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
+     res.render('index.ejs', {fish : 'sharker'})
 })
 
 app.listen(process.env.PORT || 3000, () => {
