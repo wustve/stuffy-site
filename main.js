84d7b9a3352bee713dd35a5f5f2314189d108ebf
wustve <wu.stve@gmail.com>
2020-08-21 22:37:19 -0400

    attempt to fix db

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ const { DatabaseController } = require("./database")
 require("dotenv").config()
 
 let database = new DatabaseController(process.env.DATABASE_URL);
-var result = database.select().then((res)=>{return res})
-console.log(result)
 //database.create()
 //database.client.commit()
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+     
+     database.select()
      //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
      //res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679', options :[1,2,3]})
      res.render("homepage.ejs", {stuffies: ["happyfeet", "wolfy"], imageSteven: "https://cdn.discordapp.com/attachments/515341359771680788/746501441832747019/IMG_20200821_184907.jpg", imageMonica: "https://cdn.discordapp.com/attachments/515341359771680788/746435637149696020/20200821_142806.jpg"})
 })
 
-app.get("/stuffies/:stuffyName", function(req, res) {
+app.get("/:stuffyName", function(req, res) {
      res.render("index.ejs", {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679', options :[1,2,3]})
 })
 
