b05cc1db0fa44292b6dbf851f64342ea2722aa0a
wustve <wu.stve@gmail.com>
2020-08-23 19:02:54 -0400

    adjust layout

--- database.js
+++ database.js
@@ -16,8 +16,16 @@ class DatabaseController {
 
           try {
                //await this.client.query("CREATE TABLE Stuffies (name varchar(255), animal_type varchar(255), image varchar(255), owner varchar(255), name_origin text, origin text, other_notes text);")
-               //this.client.query('INSERT INTO stuffies (name, animal_type,image,owner,origin) VALUES ($1,$2,$3,$4,$5) RETURNING *',['Angela','Bear', 'https://cdn.discordapp.com/attachments/515341359771680788/743319781771313242/20200813_000648.jpg','Monica', 'From Build-A-Bear in Square One'])
-               await this.client.query('SELECT * FROM stuffies')
+               //await this.client.query("DELETE FROM stuffies WHERE name = 'Canuck '")
+
+               //await this.client.query('INSERT INTO stuffies (name, animal_type,image,owner,origin, other_notes) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',['Angela','Bear', 'https://cdn.discordapp.com/attachments/515341359771680788/743319781771313242/20200813_000648.jpg','Monica', 'From Build-A-Bear in Square One', 'Has little sewn in heart button that plays heart beat when pressed'])
+               //await this.client.query('INSERT INTO stuffies (name, animal_type,image,owner) VALUES ($1,$2,$3,$4) RETURNING *',['Blossom','Dog', 'https://cdn.discordapp.com/attachments/515341359771680788/736389714168512643/JPEG_20200724_210934.jpg','Monica'])
+               //await this.client.query('INSERT INTO stuffies (name, animal_type,image,owner,name_origin,origin) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',['Blanchette','Bear', 'https://cdn.discordapp.com/attachments/515339628115394570/652571170884812820/IMG_20191206_130437.jpg','Steven','She white and Blanchette is like blanche but its a name','From Miniso in Erin Mills Town Centre on 2019/12/06'])
+               //await this.client.query('INSERT INTO stuffies (name, animal_type,image,owner,name_origin,other_notes) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',['Canuck','Bear', 'https://cdn.discordapp.com/attachments/515341359771680788/747223388975530024/IMG_20200823_183853.jpg','Steven','We searched for famous polar bear names and found Knut, a polar bear who lived at the Berlin Zoo. Changed it to Canuck so it was more Canadian.','Underwent surgery to seal crotch and arm wounds'])
+          
+          
+          
+          
           }
           catch(err){
                console.log(err)
--- main.js
+++ main.js
@@ -8,18 +8,18 @@ require("dotenv").config()
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
-
 app.get('/', async (req, res) => {
      //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
      //res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679', options :[1,2,3]})
-     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies')
+     thing = await new DatabaseController(process.env.DATABASE_URL).create()
+     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies ORDER BY name ASC;')
      res.render("homepage.ejs", {stuffies: ["happyfeet", "wolfy"], imageSteven: "https://cdn.discordapp.com/attachments/515341359771680788/746501441832747019/IMG_20200821_184907.jpg", imageMonica: "https://cdn.discordapp.com/attachments/515341359771680788/746435637149696020/20200821_142806.jpg", options: menuResult.rows})
 })
 
 app.get("/:stuffyName/:stuffyType", async function(req, res) {
      console.log(req.params.stuffyName)
      console.log(req.params.stuffyType)
-     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies')
+     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies ORDER BY name ASC;')
      dbResult = await new DatabaseController(process.env.DATABASE_URL).command("SELECT * FROM stuffies WHERE name = $1 AND animal_type = $2", [req.params.stuffyName, req.params.stuffyType])
      console.log(dbResult)
      if (dbResult.rowCount > 0) {
