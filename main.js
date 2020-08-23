fb0c91c4df22f093c185525ee2f46fc089659253
wustve <wu.stve@gmail.com>
2020-08-23 17:30:23 -0400

    fix menu

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', async (req, res) => {
      //res.send(ejs.render('<%= fish%>', {fish: 'shark'}))
      //res.render('index.ejs', {fish : 'tuna', image: 'https://media.discordapp.net/attachments/515341359771680788/746389232192585788/20200821_112356.jpg?width=509&height=679', options :[1,2,3]})
-     res.render("homepage.ejs", {stuffies: ["happyfeet", "wolfy"], imageSteven: "https://cdn.discordapp.com/attachments/515341359771680788/746501441832747019/IMG_20200821_184907.jpg", imageMonica: "https://cdn.discordapp.com/attachments/515341359771680788/746435637149696020/20200821_142806.jpg", options: [('agerger sdfsd','bear'),('agerger sdfsd','bear'),('agerger sdfsd','bear'),('agerger sdfsd','bear')]})
+     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies')
+     res.render("homepage.ejs", {stuffies: ["happyfeet", "wolfy"], imageSteven: "https://cdn.discordapp.com/attachments/515341359771680788/746501441832747019/IMG_20200821_184907.jpg", imageMonica: "https://cdn.discordapp.com/attachments/515341359771680788/746435637149696020/20200821_142806.jpg", options: menuResult.rows})
 })
 
 app.get("/:stuffyName/:stuffyType", async function(req, res) {
      console.log(req.params.stuffyName)
      console.log(req.params.stuffyType)
+     menuResult = await new DatabaseController(process.env.DATABASE_URL).command('Select name, animal_type FROM stuffies')
      dbResult = await new DatabaseController(process.env.DATABASE_URL).command("SELECT * FROM stuffies WHERE name = $1 AND animal_type = $2", [req.params.stuffyName, req.params.stuffyType])
      console.log(dbResult)
      if (dbResult.rowCount > 0) {
@@ -28,7 +30,8 @@ app.get("/:stuffyName/:stuffyType", async function(req, res) {
                owner: dbResult.rows[0].owner, 
                name_origin : dbResult.rows[0].name_origin, 
                origin :  dbResult.rows[0].origin, 
-               other_notes : dbResult.rows[0].other_notes
+               other_notes : dbResult.rows[0].other_notes,
+               options: menuResult.rows
           })
      }
      else {
