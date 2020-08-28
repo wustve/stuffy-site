70d589e06089bdcc387bf626522dca34a68b3478
wustve <wu.stve@gmail.com>
2020-08-27 22:30:13 -0400

    add external js

---
+++ public/darkLight.js
@@ -0,0 +1,17 @@
+function changeSiteColour() {
+     var html = document.querySelector("html");
+     var button = document.querySelector("#header > button");
+     if (button.className === "darkMode") {
+          html.setAttribute("colour-mode", "light");
+          button.setAttribute("class", "lightMode");
+          button.innerHTML = "Dark Mode"
+          localStorage.setItem("colour-mode", "light");
+     }
+     else if (button.className == "lightMode") {
+          html.setAttribute("colour-mode", "dark");
+          button.setAttribute("class", "darkMode");
+          button.innerHTML = "Light Mode"
+          localStorage.setItem("colour-mode", "dark");
+     }
+}
+
